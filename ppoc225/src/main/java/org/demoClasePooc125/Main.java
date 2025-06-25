1ba052b3c1d342370ccd9b73c11ba5e54d416cb6
Darius2912 <jchavesl@ucenfotec.ac.cr>
2025-06-25 17:56:06 -0600

    main caso de estudio

--- ppoc225/src/main/java/org/demoClasePooc125/Main.java
+++ ppoc225/src/main/java/org/demoClasePooc125/Main.java
@@ -1,17 +1,52 @@
 package org.demoClasePooc125;
+import org.demoClasePooc125.view.CursoView;
+import org.demoClasePooc125.view.GrupoView;
+import org.demoClasePooc125.view.GrupoCursoView;
+import org.demoClasePooc125.view.ProfesorView;
+import org.demoClasePooc125.view.EstudianteView;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int opcion;
+        do {
+            System.out.println("\n=== SISTEMA DE GESTIÓN ===");
+            System.out.println("1. Gestión de Grupos");
+            System.out.println("2. Gestión de Estudiantes");
+            System.out.println("3. Gestión de Profesores");
+            System.out.println("4. Gestión de Cursos");
+            System.out.println("5. Relación Grupo-Curso");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+            sc.nextLine(); // limpiar buffer
+
+            switch (opcion) {
+                case 1:
+                    GrupoView.main(null);
+                    break;
+                case 2:
+                    EstudianteView.main(null);
+                    break;
+                case 3:
+                    ProfesorView.main(null);
+                    break;
+                case 4:
+                    CursoView.main(null);
+                    break;
+                case 5:
+                    GrupoCursoView.main(null);
+                    break;
+                case 0:
+                    System.out.println("Saliendo del sistema...");
+                    break;
+                default:
+                    System.out.println("Opción inválida.");
+            }
+        } while (opcion != 0);
     }
 }
